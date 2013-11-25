9fac5a0274f6b3784e0e7c1242814f6f5004e565
zestos <zestos@gmail.com>
2013-11-25 13:38:01 -0800

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roobot::Application.config.secret_token = '84f58554941374f11673f3db4dffe407ef440c6a8e8cd6f1bf07b2c8871f07250d15ec299c082efd86ec7398e7f3e68c136707a1a54024ffb04bd60ccaf65d05'
