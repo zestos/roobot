6942c9aeffff7d428083987ad88340536bb247f3
zestos <zestos@gmail.com>
2013-12-06 09:40:54 -0800

    Move secret token to .ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Roobot::Application.config.secret_token = '84f58554941374f11673f3db4dffe407ef440c6a8e8cd6f1bf07b2c8871f07250d15ec299c082efd86ec7398e7f3e68c136707a1a54024ffb04bd60ccaf65d05'
+
+if Rails.env.development? or Rails.env.test?
+  Roobot::Application.config.secret_token = ('x' * 30)
+  # meets minimum requirement of 30 chars long
+else
+  Roobot::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
