8a2c3c9f7ea1218122ac73b93d99a0332cf63cdd
zestos <zestos@gmail.com>
2013-12-06 17:21:04 -0800

    debug files

--- app/workers/backup_worker.rb
+++ app/workers/backup_worker.rb
@@ -8,10 +8,13 @@ class BackupWorker
 
     dropbox_client = DropboxClient.new(dropbox_access_token)
 
-    Dir.mkdir(File.join(Rails.root, 'tmp'))
+    # Dir.mkdir(File.join(Rails.root, 'tmp'))
 
     image_urls.each do |url|
       remote_image = open(url).read
+
+      binding.pry
+
       dropbox_client.put_file("#{DateTime.now.to_s}.jpg", remote_image)
     end
 
