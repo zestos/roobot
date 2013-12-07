06e0a97b532c85f70e022d07b0dfb661a8411e87
zestos <zestos@gmail.com>
2013-12-06 16:32:28 -0800

    Modified Instagram callback URL

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,6 +1,6 @@
 class OauthController < ApplicationController
 
-CALLBACK_URL = "http://localhost:3000/oauth/callback"
+CALLBACK_URL = "http://roobotapp.herokuapp.com/oauth/callback"
 
   def connect
     redirect_to Instagram.authorize_url(redirect_uri: CALLBACK_URL)
